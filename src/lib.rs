540efa66e9a509e9ac8ca72069baed63f292e43c
schaeff <thibaut@schaeff.fr>
2018-08-27 18:30:32 +0200

    split code, add structs for assigned var and lincomb, remove clones, add function args

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(box_syntax, box_patterns)]
+
 extern crate bellman;
 extern crate pairing;
 extern crate rand;
@@ -8,7 +10,8 @@ use rand::{thread_rng};
 // Bring in some tools for using pairing-friendly curves
 use pairing::{
     Engine,
-    Field
+    Field,
+    PrimeField,
 };
 
 // We're going to use the BLS12-381 pairing-friendly elliptic curve.
@@ -22,6 +25,7 @@ use bellman::{
     ConstraintSystem,
     SynthesisError,
     Variable,
+    LinearCombination,
 };
 
 // We're going to use the Groth16 proving system.
@@ -36,168 +40,233 @@ use bellman::groth16::{
 
 use std::collections::{HashMap};
 
+// data structure to hold a FieldElement variable with its value
+// it's fine to clone these
+#[derive(Clone)]
+struct AssignedVariable<E: Engine> {
+	variable: Variable,
+	value: E::Fr
+}
+
+#[derive(Clone)]
+struct AssignedLinearCombination<E: Engine> {
+	combination: LinearCombination<E>,
+	value: E::Fr
+}
+
+impl<E: Engine> From<AssignedVariable<E>> for AssignedLinearCombination<E> {
+	fn from(assigned_variable: AssignedVariable<E>) -> Self {
+		AssignedLinearCombination {
+			combination: LinearCombination::zero() + assigned_variable.variable,
+			value: assigned_variable.value
+		}
+	}
+}
+
+// data structures for a program
+// we don't want to clone these as they contain strings
 struct Program<E: Engine> {
 	main: Function<E>,
 	functions: Vec<Function<E>>
 }
 
-#[derive(Clone)]
 struct Function<E: Engine> {
 	id: String,
-	statements: Vec<Statement<E>>
+	arguments: Vec<String>,
+	statements: Vec<Statement<E>>,
 }
 
-#[derive(Clone)]
 enum Statement<E: Engine> {
 	Definition(String, Expression<E>),
 	Return(Expression<E>)
 }
 
-#[derive(Clone)]
 enum Expression<E: Engine> {
 	Number(E::Fr),
+	Add(Box<Expression<E>>, Box<Expression<E>>),
 	Identifier(String),
-	FunctionCall(String) // no arguments, one return value
+	FunctionCall(String, Vec<Expression<E>>) // no arguments, one return value
+}
+
+impl<E: Engine> Expression<E> {
+	fn flatten<CS: ConstraintSystem<E>>(
+		&self,
+		cs: &mut CS,
+		context: &Vec<Function<E>>,
+		symbols: &mut HashMap<String, AssignedLinearCombination<E>>
+	) -> Result<AssignedLinearCombination<E>, SynthesisError>
+	{
+		match *self {
+			Expression::Number(n) => {
+				let var = cs.alloc(|| n.to_string(), || {
+					Ok(n)
+				})?;
+
+				Ok(AssignedVariable { variable: var, value: n }.into())
+			},
+			Expression::Identifier(ref id) => {
+				let assigned_variable = symbols.get(id).ok_or(SynthesisError::AssignmentMissing)?;
+
+				Ok((*assigned_variable).clone())
+			},
+			Expression::FunctionCall(ref f_id, ref arguments) => {
+
+				let fun = context.iter().find(|f| &f.id == f_id).expect("undefined function");
+
+				let mut inputs = vec![];
+				for arg in arguments {
+					let assigned_variable = arg.flatten(
+						cs,
+						context,
+						symbols
+					)?;
+
+					inputs.push(assigned_variable);
+				}		
+
+				let assigned_output = fun.flatten(
+					cs,
+					context,
+					&inputs,
+				)?;
+
+				Ok(assigned_output)
+			},
+			Expression::Add(ref a, ref b) => {
+				// naive approach: create a new wire for each term in the sum
+
+				let assigned_variable_a = a.flatten(
+					cs, 
+					context,
+					symbols
+				)?;
+
+				let assigned_variable_b = b.flatten(
+					cs, 
+					context,
+					symbols,
+				)?;
+
+				let mut res = assigned_variable_a.value;
+				res.add_assign(&assigned_variable_b.value);
+
+				let sum = cs.alloc(|| format!("sum"), || {
+					Ok(res)
+				})?;
+
+				// CONSTRAINT
+				cs.enforce(
+	                || "res = a + b",
+	                |lc| lc + sum,
+	                |lc| lc + CS::one(),
+	                |lc| lc + &assigned_variable_a.combination + &assigned_variable_b.combination
+	            );
+
+	            Ok(AssignedVariable { variable: sum, value: res }.into())
+			}
+		}
+	}
+}
+
+impl<E: Engine> Statement<E> {
+	fn flatten<CS: ConstraintSystem<E>>(
+		&self, 
+		cs: &mut CS,
+		context: &Vec<Function<E>>,
+		symbols: &mut HashMap<String, AssignedLinearCombination<E>>,
+		is_main: bool,
+	) -> Result<Option<AssignedLinearCombination<E>>, SynthesisError>
+	{
+		match *self {
+			Statement::Definition(ref id, ref e) => {
+				// condense right side into one wire with a value
+				let assigned_variable = e.flatten(
+					cs,
+					context,
+					symbols
+				)?;
+
+				// no need to create a new variable, just register `id`is being worth e.flattened
+
+	            symbols.insert(id.clone(), assigned_variable);
+
+	            Ok(None)
+			},
+			Statement::Return(ref e) => {
+				// condense right side into one wire with a value
+				let assigned_variable = e.flatten(
+					cs,
+					context,
+					symbols
+				)?;
+
+				let out = match is_main {
+					// if we're in the main function, we need to make the return variable a public input in the CS
+					true => {
+						let out = cs.alloc_input(|| "out", || {
+							Ok(assigned_variable.value)
+						})?;
+
+						// CONSTRAINT
+						cs.enforce(
+			                || "out = e * ~one",
+			                |lc| lc + &assigned_variable.combination,
+			                |lc| lc + CS::one(),
+			                |lc| lc + out
+			            );
+
+			            AssignedVariable { variable: out, value: assigned_variable.value}.into()
+					},
+					// otherwise, we already have everything we need to return
+					false => {
+						assigned_variable
+					}
+				};
+
+	            Ok(Some(out))
+			}
+		}
+	}
 }
 
 impl<E: Engine> Function<E> {
-	fn synthesize_with_context<CS: ConstraintSystem<E>>(
-		self,
+	fn flatten<CS: ConstraintSystem<E>>(
+		&self,
 		cs: &mut CS,
-		context: &mut Vec<Function<E>>
-	) -> Result<(Variable, E::Fr), SynthesisError> 
+		context: &Vec<Function<E>>,
+		arguments: &Vec<AssignedLinearCombination<E>>,
+	) -> Result<AssignedLinearCombination<E>, SynthesisError> 
 	{
 		let mut cs = &mut cs.namespace(|| format!("{}", self.id));
 
 		// map from identifier to CS wire
 		let mut symbols = HashMap::new();
-		// map from identifier to value 
-		let mut witness = HashMap::new();
 
-		for statement in self.statements.clone() {
-			match statement {
-				Statement::Definition(id, e) => {
-					match e {
-						Expression::Number(n) => {
-							// SOLVER
+		for (i, assigned_combination) in arguments.iter().enumerate() {
+			symbols.insert(self.arguments[i].clone(), (*assigned_combination).clone());
 
-							// store the value of the identifier in the witness
-							witness.insert(id.clone(), n); // doing the same thing as cs.alloc?
+			let input = cs.alloc(|| format!("input {}", i), || {
+				Ok(assigned_combination.value)
+			})?;
 
-							let var = cs.alloc(|| id.clone(), || {
-								Ok(n) // should just be getting it from the witness?
-							})?;
+			cs.enforce(
+				|| "arg = passed_arg",
+				|lc| lc + input, 
+				|lc| lc + CS::one(),
+				|lc| lc + &assigned_combination.combination,
+			);
+		}
 
-							// store the CS element against the name for future reference
-				            symbols.insert(id, var);
+		let is_main = self.id == "main";
 
-							// CONSTRAINT
-							cs.enforce(
-				                || "id = n * ~one",
-				                |lc| lc + (n, CS::one()),
-				                |lc| lc + CS::one(),
-				                |lc| lc + var
-				            );
-						},
-						Expression::FunctionCall(f_id) => {
-							// SOLVER
-							// find the wires for the called function's return value
-
-							let fun = context.iter().find(|f| f.id == f_id).expect("undefined function").clone();
-
-							let (output_wire, output_val) = fun.clone().synthesize_with_context(
-								&mut cs,
-								context
-							)?;
-
-							// store the value of the identifier in the witness
-							witness.insert(id.clone(), output_val); // doing the same thing as cs.alloc?
-
-							let var = cs.alloc(|| id.clone(), || {
-								Ok(output_val) // should just be getting it from the witness?
-							})?;
-
-							// store the CS element against the name for future reference
-				            symbols.insert(id, output_wire);
-
-							// CONSTRAINT
-							cs.enforce(
-				                || "id = f()",
-				                |lc| lc + output_wire,
-				                |lc| lc + CS::one(),
-				                |lc| lc + var
-				            );
-						},
-						_ => unimplemented!()
-					}
-				},
-				Statement::Return(e) => {
-					match e {
-						Expression::Number(n) => {
-							// SOLVER
-							let out = match &self.id == "main" {
-								// if we're in the main function, we need to make the return variable a public input in the CS
-								true => {
-									cs.alloc_input(|| "out", || {
-										Ok(n)
-									})
-								},
-								false => {
-									cs.alloc(|| "out", || {
-										Ok(n)
-									})
-								}
-							}?;
-
-							// CONSTRAINT
-							cs.enforce(
-				                || "out = n * ~one",
-				                |lc| lc + (n, CS::one()),
-				                |lc| lc + CS::one(),
-				                |lc| lc + out
-				            );
-
-				            // export to Abi for this function to enable calling it!
-				            context.push(self.clone());
-				            return Ok((out, n));
-						},
-						Expression::Identifier(id) => {
-							// SOLVER
-							// find the CS variable for that id
-							let var = symbols.get(&id).expect("semantic error, variable should be defined");
-							let value = witness.get(&id).ok_or(SynthesisError::AssignmentMissing)?;
-
-							let out = match &self.id == "main" {
-								// if we're in the main function, we need to make the return variable a public input in the CS
-								true => {
-									cs.alloc_input(|| "out", || {
-										Ok(*value)
-									})
-								},
-								false => {
-									cs.alloc(|| "out", || {
-										Ok(*value)
-									})
-								}
-							}?;
-
-							// CONSTRAINT
-							cs.enforce(
-				                || "out = id",
-				                |lc| lc + *var,
-				                |lc| lc + CS::one(),
-				                |lc| lc + out
-				            );
-
-				       		// export to Abi for this function to enable calling it!
-				            context.push(self.clone()); // we don't cache yet so we'll have to flattened from scratch
-				            return Ok((out, *value));
-						},
-						_ => unimplemented!()
-					}
-				}
+		for statement in self.statements.iter() {
+			match statement.flatten(&mut cs, &context, &mut symbols, is_main) {
+				Ok(Some(res)) => return Ok(res),
+				Ok(None) => {},
+				Err(e) => return Err(e)
 			}
 		}
+
 		Err(SynthesisError::AssignmentMissing)
 	}
 }
@@ -208,13 +277,15 @@ impl<E: Engine> Circuit<E> for Program<E> {
         cs: &mut CS
     ) -> Result<(), SynthesisError>
     {
-    	let mut context = vec![];
-    	let res: Vec<_> = self.functions.into_iter().map(|f| f.synthesize_with_context(cs, &mut context)).collect();
-        match self.main.synthesize_with_context(cs, &mut context) {
+        match self.main.flatten(cs, &self.functions, &vec![]) {
         	Ok(..) => Ok(()),
         	Err(e) => Err(e)
         }
     }
+}
+
+fn number_to_fr<E: Engine>(number: usize) -> E::Fr {
+	E::Fr::from_str(&number.to_string()).unwrap()
 }
 
 #[test]
@@ -223,27 +294,31 @@ fn test_program() {
 
     // our toy program
     // def main():
-    //    a = foo()
-    //    return a
+    //    a = foo(3)
+    //    return a + 42
     //
-    // def foo():
-    //    return 1
+    // def foo(b):
+    //    return b + 1
+    //
+    // should return 43
     let program = Program::<Bls12> {
 		main: Function {
 			id: "main".to_string(),
+			arguments: vec![],
 			statements: vec![
-				Statement::Definition(String::from("a"), Expression::FunctionCall("foo".to_string())),
+				Statement::Definition(String::from("a"), Expression::FunctionCall("foo".to_string(), vec![Expression::Number(number_to_fr::<Bls12>(3))])),
 				Statement::Return(
-					Expression::Identifier(String::from("a"))
+					Expression::Add(box Expression::Identifier(String::from("a")), box Expression::Number(number_to_fr::<Bls12>(42)))
 				)
 			]
 		},
 		functions: vec![
 			Function {
 				id: "foo".to_string(),
+				arguments: vec!["b".to_string()],
 				statements: vec![
 					Statement::Return(
-						Expression::Number(<Bls12 as Engine>::Fr::one())
+						Expression::Add(box Expression::Identifier(String::from("b")), box Expression::Number(number_to_fr::<Bls12>(1)))
 					)
 				]
 			}
@@ -265,19 +340,21 @@ fn test_program() {
     let computation = Program::<Bls12> {
 		main: Function {
 			id: "main".to_string(),
+			arguments: vec![],
 			statements: vec![
-				Statement::Definition(String::from("a"), Expression::FunctionCall("foo".to_string())),
+				Statement::Definition(String::from("a"), Expression::FunctionCall("foo".to_string(), vec![Expression::Number(number_to_fr::<Bls12>(3))])),
 				Statement::Return(
-					Expression::Identifier(String::from("a"))
+					Expression::Add(box Expression::Identifier(String::from("a")), box Expression::Number(number_to_fr::<Bls12>(42)))
 				)
 			]
 		},
 		functions: vec![
 			Function {
 				id: "foo".to_string(),
+				arguments: vec!["b".to_string()],
 				statements: vec![
 					Statement::Return(
-						Expression::Number(<Bls12 as Engine>::Fr::one())
+						Expression::Add(box Expression::Identifier(String::from("b")), box Expression::Number(number_to_fr::<Bls12>(1)))
 					)
 				]
 			}
@@ -295,6 +372,6 @@ fn test_program() {
     assert!(verify_proof(
         &pvk,
         &proof,
-        &[<Bls12 as Engine>::Fr::one()]
+        &[number_to_fr::<Bls12>(46)]
     ).unwrap());
 }
